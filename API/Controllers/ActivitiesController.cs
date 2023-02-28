1b8beacb631ee3c87292078f3e73f5fb0bb4199b
lmochoa <hitmaker05151984@gmail.com>
2023-02-28 11:15:55 +0800

    End of Section 2

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -6,24 +6,24 @@ using Persistence;
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
-    {    
-            private readonly DataContext _context;
-            public ActivitiesController(DataContext context)
-            {
-                _context = context;
-               
-            }
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
 
-            [HttpGet]
-            public async Task<ActionResult<List<Activity>>> GetActivities()
-            {
-                return await _context.Activities.ToListAsync();
-            }
+        }
 
-            [HttpGet("{id}")]
-            public async Task<ActionResult<Activity>> GetActivity(Guid id)
-            {
-                return await _context.Activities.FindAsync(id);
-            }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
         }
     }
+}
